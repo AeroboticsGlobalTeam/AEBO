6e03edbef138361eacdfaf7099cb9968a78e0cbf
AeroboticsGlobalTeam <52552510+AeroboticsGlobalTeam@users.noreply.github.com>
2021-08-04 12:50:00 +1000

    Added LCD functions

--- src/AEBO.cpp
+++ src/AEBO.cpp
@@ -6,12 +6,19 @@
 
  #include "Arduino.h"
  #include "AEBO.h"
+ #include <inttypes.h>
+ #include "Wire.h"
+ #include "Print.h"
+ 
+
+
 
  AEBO::AEBO(){
   pinMode(12,OUTPUT);
   pinMode(13,OUTPUT);
   pinMode(3,OUTPUT);
   pinMode(11,OUTPUT);
+  
  }
 
 void AEBO::Forward(int speed){
@@ -22,8 +29,8 @@ void AEBO::Forward(int speed){
 }
 
 void AEBO::Backward(int speed){
-  digitalWrite(12,HIGH);
-  digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  digitalWrite(13,LOW);
   analogWrite(3,speed);
   analogWrite(11,speed);
 }
@@ -48,9 +55,9 @@ void AEBO::StopRobot(){
 }
 
 float AEBO::distance(int trigPin, int echoPin){
+  
   pinMode(trigPin,OUTPUT);
   pinMode(echoPin,INPUT);
-
   digitalWrite(trigPin,LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin,HIGH);
@@ -62,3 +69,148 @@ float AEBO::distance(int trigPin, int echoPin){
 
   return d;
 }
+
+//LCD functions
+
+
+
+#define printIIC(args)	Wire.write(args)
+inline size_t AEBO::write(uint8_t value) {
+	send(value, Rs);
+	return 1;
+}
+
+void AEBO::LCD(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows){
+  _Addr = lcd_Addr;
+  _cols = lcd_cols;
+  _rows = lcd_rows;
+  _backlightval = LCD_NOBACKLIGHT;
+  Wire.begin();
+	_displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
+	begin(_cols, _rows);
+}
+
+void AEBO::begin(uint8_t cols, uint8_t lines, uint8_t dotsize){
+  if (lines > 1) {
+		_displayfunction |= LCD_2LINE;
+	}
+	_numlines = lines;
+
+	// for some 1 line displays you can select a 10 pixel high font
+	if ((dotsize != 0) && (lines == 1)) {
+		_displayfunction |= LCD_5x10DOTS;
+	}
+
+	// SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
+	// according to datasheet, we need at least 40ms after power rises above 2.7V
+	// before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
+	delay(50); 
+  
+	// Now we pull both RS and R/W low to begin commands
+	expanderWrite(_backlightval);	// reset expanderand turn backlight off (Bit 8 =1)
+	delay(1000);
+
+  	//put the LCD into 4 bit mode
+	// this is according to the hitachi HD44780 datasheet
+	// figure 24, pg 46
+	
+	  // we start in 8bit mode, try to set 4 bit mode
+   write4bits(0x03 << 4);
+   delayMicroseconds(4500); // wait min 4.1ms
+   
+   // second try
+   write4bits(0x03 << 4);
+   delayMicroseconds(4500); // wait min 4.1ms
+   
+   // third go!
+   write4bits(0x03 << 4); 
+   delayMicroseconds(150);
+   
+   // finally, set to 4-bit interface
+   write4bits(0x02 << 4); 
+
+
+	// set # lines, font size, etc.
+	command(LCD_FUNCTIONSET | _displayfunction);  
+	
+	// turn the display on with no cursor or blinking default
+	_displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
+	display();
+	
+	// clear it off
+	LCD_clear();
+	
+	// Initialize to default text direction (for roman languages)
+	_displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+	
+	// set the entry mode
+	command(LCD_ENTRYMODESET | _displaymode);
+	
+	command(LCD_RETURNHOME);  // set cursor position to zero
+	delayMicroseconds(2000);  // this command takes a long time!
+
+	_displaycontrol |= LCD_CURSORON;
+	command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+void AEBO::command(uint8_t value) {
+	send(value, 0);
+}
+
+void AEBO::display() {
+	_displaycontrol |= LCD_DISPLAYON;
+	command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+
+void AEBO::LCD_clear(){
+  command(LCD_CLEARDISPLAY);// clear display, set cursor position to zero
+	delayMicroseconds(2000);  // this command takes a long time!
+}
+
+void AEBO:: LCD_setCursor(uint8_t col, uint8_t row){
+	int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
+	if ( row > _numlines ) {
+		row = _numlines-1;    // we count rows starting w/0
+	}
+	command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
+}
+
+void AEBO::LCD_backlight(){
+  _backlightval=LCD_BACKLIGHT;
+	expanderWrite(0);
+}
+
+void AEBO::send(uint8_t value, uint8_t mode) {
+	uint8_t highnib=value&0xf0;
+	uint8_t lownib=(value<<4)&0xf0;
+       write4bits((highnib)|mode);
+	write4bits((lownib)|mode); 
+}
+
+void AEBO::write4bits(uint8_t value) {
+	expanderWrite(value);
+	pulseEnable(value);
+}
+
+void AEBO::expanderWrite(uint8_t _data){                                        
+	Wire.beginTransmission(_Addr);
+	printIIC((int)(_data) | _backlightval);
+	Wire.endTransmission();   
+}
+
+void AEBO::pulseEnable(uint8_t _data){
+	expanderWrite(_data | En);	// En high
+	delayMicroseconds(1);		// enable pulse must be >450ns
+	
+	expanderWrite(_data & ~En);	// En low
+	delayMicroseconds(50);		// commands need > 37us to settle
+} 
+void AEBO::LCD_print(const char c[]){
+	//This function is not identical to the function used for "real" I2C displays
+	//it's here so the user sketch doesn't have to be changed 
+	print(c);
+}
+
+
+
--- src/AEBO.h
+++ src/AEBO.h
@@ -9,9 +9,58 @@
 #define AEBO_H
 
 #include "Arduino.h"
+#include <inttypes.h>
+#include "Print.h" 
+#include <Wire.h>
+
+// commands
+#define LCD_CLEARDISPLAY 0x01
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT 0x10
+#define LCD_FUNCTIONSET 0x20
+#define LCD_SETCGRAMADDR 0x40
+#define LCD_SETDDRAMADDR 0x80
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 0x00
+#define LCD_ENTRYLEFT 0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
+// flags for display on/off control
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00
+#define LCD_CURSORON 0x02
+#define LCD_CURSOROFF 0x00
+#define LCD_BLINKON 0x01
+#define LCD_BLINKOFF 0x00
+
+// flags for display/cursor shift
+#define LCD_DISPLAYMOVE 0x08
+#define LCD_CURSORMOVE 0x00
+#define LCD_MOVERIGHT 0x04
+#define LCD_MOVELEFT 0x00
+
+// flags for function set
+#define LCD_8BITMODE 0x10
+#define LCD_4BITMODE 0x00
+#define LCD_2LINE 0x08
+#define LCD_1LINE 0x00
+#define LCD_5x10DOTS 0x04
+#define LCD_5x8DOTS 0x00
+
+// flags for backlight control
+#define LCD_BACKLIGHT 0x08
+#define LCD_NOBACKLIGHT 0x00
+
+#define En B00000100  // Enable bit
+#define Rw B00000010  // Read/Write bit
+#define Rs B00000001  // Register select bit
 
 
-class AEBO{
+class AEBO : public Print{
 
   public:
     AEBO();
@@ -26,10 +75,34 @@ class AEBO{
     //Distance Function
     float distance(int trigPin, int echoPin);
 
+    //LCD functions
+
+    void LCD(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows);
+    void LCD_clear();
+    void LCD_backlight();
+    void LCD_setCursor(uint8_t, uint8_t);
+    virtual size_t write(uint8_t);
+    void LCD_print(const char c[]);
+
    private:
 
     int _trigpin;
     int _echopin;
+    void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
+    void send(uint8_t, uint8_t);
+    void write4bits(uint8_t);
+    void expanderWrite(uint8_t);
+    void pulseEnable(uint8_t);
+    void display();
+    void command(uint8_t);
+    uint8_t _Addr;
+    uint8_t _displayfunction;
+    uint8_t _displaycontrol;
+    uint8_t _displaymode;
+    uint8_t _numlines;
+    uint8_t _cols;
+    uint8_t _rows;
+    uint8_t _backlightval;
     
     
 };
